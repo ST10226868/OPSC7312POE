c2c4deaa0fd015896a2c264d48842d8dfdd8f3ea
lindo <ST10021160@vcconnect.edu.za>
2024-10-21 15:30:24 +0200

    Calender, Front and back end, no sync

--- app/src/main/java/com/example/opsc7312poe/CalenderFragment.kt
+++ app/src/main/java/com/example/opsc7312poe/CalenderFragment.kt
@@ -1,25 +1,25 @@
 package com.example.opsc7312poe
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.TimePicker
+import androidx.fragment.app.Fragment
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CalenderFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CalenderFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var datePicker: DatePicker
+    private lateinit var timePicker: TimePicker
+    private lateinit var descriptionEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,19 +34,52 @@ class CalenderFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calender, container, false)
+        val view = inflater.inflate(R.layout.fragment_calender, container, false)
+
+        // Initialize the DatePicker, TimePicker, and EditText
+        datePicker = view.findViewById(R.id.datePicker)
+        timePicker = view.findViewById(R.id.timePicker)
+        descriptionEditText = view.findViewById(R.id.edit_text_description)
+
+        // Set 24-hour view for TimePicker
+        timePicker.setIs24HourView(true)
+
+        // Get the current date and time
+        val calendar = Calendar.getInstance()
+        val currentYear = calendar.get(Calendar.YEAR)
+        val currentMonth = calendar.get(Calendar.MONTH)
+        val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
+        val currentHour = calendar.get(Calendar.HOUR_OF_DAY)
+        val currentMinute = calendar.get(Calendar.MINUTE)
+
+        // Set the current date in DatePicker
+        datePicker.updateDate(currentYear, currentMonth, currentDay)
+
+        // Set the current time in TimePicker
+        timePicker.hour = currentHour
+        timePicker.minute = currentMinute
+
+        return view
+    }
+
+    // Function to get the selected date and time as a string
+    fun getSelectedDateTime(): String {
+        val day = datePicker.dayOfMonth
+        val month = datePicker.month + 1  // Month is 0-indexed
+        val year = datePicker.year
+
+        val hour = timePicker.hour
+        val minute = timePicker.minute
+
+        return String.format("%02d/%02d/%d %02d:%02d", day, month, year, hour, minute)
+    }
+
+    // Function to get the description text entered by the user
+    fun getDescription(): String {
+        return descriptionEditText.text.toString()
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalenderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             CalenderFragment().apply {
